bdf70d95ffcb1ae0baf3d422441a51a27d6ee725
victorsheepman <chirlychplin@gmail.com>
2022-06-28 10:08:25 -0400

    1.3

---
+++ src/style/base.js
@@ -0,0 +1,31 @@
+import {Dimensions} from 'react-native'
+
+export const dimensions = {
+  fullHeight: Dimensions.get('window').height,
+  fullWidth: Dimensions.get('window').width,
+  small:'40%',
+}
+  
+export const colors  = {
+  gold: '#bea741',
+  black: '#000000',
+  white: '#ffffff',
+  red:'#ce0000',
+  grey:'#73777B',
+  green:'#00ce3f',
+  blue:'#392afa'
+}
+
+export const padding = {
+  sm: 10,
+  md: 20,
+  lg: 30,
+  xl: 40
+}
+
+export const fonts = {
+  sm: 12,
+  md: 18,
+  lg: 28,
+  primary: 'Cochin'
+}
